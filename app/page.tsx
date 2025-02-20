d2d3d8b6fc55984276f191d803f49cf88ac0b477
Joost Meijles <joost.meijles@avivasolutions.nl>
2025-02-20 10:44:07 +0100

    Use endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 // force-static to generate this as ISR function
-export const revalidate = 1;
+export const dynamic = 'force-static';
 
 export default async function Home() {
-  // const res = await fetch('http://localhost:3000/api/data', {
-  //   next: {
-  //     tags: ['homepage']
-  //   }
-  // });
+  const res = await fetch('https://stale-page-not-served.vercel.app//api/data', {
+    next: {
+      tags: ['homepage']
+    }
+  });
 
-  // const json = await res.json();
+  const json = await res.json();
 
-  // return <span>Homepage: {json.timestamp}</span>;
-  return <span>Test</span>
+  return <span>Homepage: {json.timestamp}</span>;
 }
